5b5bc4a7e05f59c9ba4d97399f9de86a3689194b
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-02-24 21:11:31 +0200

    Cart service

---
+++ src/app/shared/services/cart/cart.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { api_url } from '../../../core/custom_injections/api_url';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  _httpClient = inject(HttpClient)
+  token:string = JSON.stringify( localStorage.getItem("userToken"))    
+
+
+  constructor(@Inject(api_url) private apiPath:string) { }
+
+
+  addProductToCard(productId:string): Observable<any>
+  {
+    return this._httpClient.post(this.apiPath + `/cart`, { productId }, {
+      headers: {
+        token: this.token
+      }
+    })
+  }
+
+  updateProductquantity(productId: string, count:string): Observable<any>
+  {
+    return this._httpClient.put(this.apiPath + `/cart ${productId }`,  { count }, {
+      headers: {
+        token: this.token
+      }
+    })
+  }
+
+  getCard(): Observable<any> {
+    return this._httpClient.get(this.apiPath + `/cart`, {
+      headers: {
+        token: this.token
+      }
+    })
+  }
+
+  RemoveSpecificIItem(productId: string): Observable<any> {
+    return this._httpClient.delete(this.apiPath + `/cart ${productId}`, {
+      headers: {
+        token: this.token
+      }
+    })
+  }
+
+  clearCart(): Observable<any> {
+    return this._httpClient.delete(this.apiPath + `/cart `, {
+      headers: {
+        token: this.token
+      }
+    })
+  }
+
+
+
+
+
+
+
+}
