154345848a9fe443755ccc089596ed6911c1accd
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-03-02 22:40:34 +0200

    Prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  {
+    path: "checkout/:cartId",
+    renderMode:RenderMode.Server
+  },
+  
+  {
+    path: "productDetails/:id",
+    renderMode:RenderMode.Server
+  },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,28 @@ import { authGuard } from './core/guards/auth/auth.guard';
 import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './core/layout/auth-layout/auth-layout.component';
 import { loggedUserGuard } from './core/guards/auth/logged-user.guard';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
 	{
 		path: "auth", component: AuthLayoutComponent, children:
 		[
-			{ path: "", pathMatch: "full", canActivate: [loggedUserGuard], loadComponent: () => import('./core/auth/register/register.component').then(c => c.RegisterComponent)},
-			{ path: "login", canActivate: [loggedUserGuard], loadComponent: () => import('./core/auth/login/login.component').then(c => c.LoginComponent)},
-			{ path: "forget-password", canActivate: [loggedUserGuard], loadComponent: () => import('./core/auth/forget-password/forget-password.component').then(c => c.ForgetPasswordComponent)}
+				{ path: "", canActivate: [loggedUserGuard], loadComponent: () => import('./core/auth/register/register.component').then(c => c.RegisterComponent), data: { renderMode: RenderMode.Prerender } },
+				{ path: "login", canActivate: [loggedUserGuard], loadComponent: () => import('./core/auth/login/login.component').then(c => c.LoginComponent), data: { renderMode: RenderMode.Prerender } },
+				{ path: "forget-password", canActivate: [loggedUserGuard], loadComponent: () => import('./core/auth/forget-password/forget-password.component').then(c => c.ForgetPasswordComponent), data: { renderMode: RenderMode.Prerender } }
 		]
 	},
 
-	{ path: "home", canActivate: [authGuard] ,loadComponent: () => import('./features/pages/home/home.component').then(c => c.HomeComponent)},
-	{ path: "brands", canActivate: [authGuard] , loadComponent: () => import('./features/pages/brands/brands.component').then(c => c.BrandsComponent)},
+	{ path: "home", canActivate: [authGuard], loadComponent: () => import('./features/pages/home/home.component').then(c => c.HomeComponent), data: { renderMode: RenderMode.Prerender } },
+	{ path: "brands", canActivate: [authGuard], loadComponent: () => import('./features/pages/brands/brands.component').then(c => c.BrandsComponent), data: { renderMode: RenderMode.Prerender } },
+	{ path: "products", canActivate: [authGuard], loadComponent: () => import('./features/pages/products/products.component').then(c => c.ProductsComponent), data: { renderMode: RenderMode.Prerender } },
+	{ path: "categories", canActivate: [authGuard], loadComponent: () => import('./features/pages/categories/categories.component').then(c => c.CategoriesComponent), data: { renderMode: RenderMode.Prerender } },
 	{ path: "cart", canActivate: [authGuard] , loadComponent: () => import('./features/pages/cart/cart.component').then(c => c.CartComponent)},
-	{ path: "products", canActivate: [authGuard] , loadComponent: () => import('./features/pages/products/products.component').then(c => c.ProductsComponent)},
-	{ path: "categories", canActivate: [authGuard] , loadComponent: () => import('./features/pages/categories/categories.component').then(c => c.CategoriesComponent) },
-	{ path: "checkout/:cartId", canActivate: [authGuard] , loadComponent: () => import('./features/pages/checkout/checkout.component').then(c => c.CheckoutComponent) },
-	{ path: "productDetails/:id", canActivate: [authGuard] , loadComponent: () => import('./features/pages/product-details/product-details.component').then(c => c.ProductDetailsComponent) },
-	{ path: "allorders", canActivate: [authGuard] , loadComponent: () => import('./features/pages/orders/orders.component').then(c => c.OrdersComponent) },
+	{ path: "checkout/:cartId", canActivate: [authGuard], loadComponent: () => import('./features/pages/checkout/checkout.component').then(c => c.CheckoutComponent), data: { renderMode: RenderMode.Prerender } },
+	{ path: "productDetails/:id", canActivate: [authGuard], loadComponent: () => import('./features/pages/product-details/product-details.component').then(c => c.ProductDetailsComponent), data: { renderMode: RenderMode.Prerender } },
+	{ path: "allorders", canActivate: [authGuard], loadComponent: () => import('./features/pages/orders/orders.component').then(c => c.OrdersComponent), data: { renderMode: RenderMode.Prerender } },
 	
-	{ path: "**", loadComponent: () => import('./core/pages/not-found/not-found.component').then(c => c.NotFoundComponent)},
+	{ path: "**", loadComponent: () => import('./core/pages/not-found/not-found.component').then(c => c.NotFoundComponent), data: { renderMode: RenderMode.Prerender } },
 	
 
 ];
