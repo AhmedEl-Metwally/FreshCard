079c7d1f4238910777ebed40db1876d9dbec4157
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-03-05 22:41:11 +0200

    Add Products

--- src/app/features/pages/products/products.component.ts
+++ src/app/features/pages/products/products.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductService } from '../../../shared/services/product/product.service';
+import { Product } from '../../../shared/interfaces/product';
+import { ProductItemComponent } from "../../../shared/components/ui/product-item/product-item.component";
+import { CartService } from '../../../shared/services/cart/cart.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ProductItemComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  private readonly _productService = inject(ProductService)
+  private readonly _cartService = inject(CartService)
+   private readonly _toastrService = inject(ToastrService)
+
+   Products! : Product[]
+
+  ngOnInit(): void {
+    this.getproducts()
+  }
+
+  getproducts() {
+    this._productService.getproducts().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.Products = res.data
+      },
+      error(err) {
+        console.log(err);
+      },
+      complete() {
+        console.log("complete");
+      }
+    })
+  }
+
+  addToCard(id: string) {
+    this._cartService.addProductToCard(id).subscribe
+      ({
+        next: (res) => {
+          console.log(res);
+          this._toastrService.success(res.message, 'Hii');
+        }
+      })
+  }
 }
